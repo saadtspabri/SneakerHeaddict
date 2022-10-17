044afa40136621d4cc6ca39697cf5037fef9b76f
saadtspabri <104324729+saadtspabri@users.noreply.github.com>
2022-10-17 21:10:16 +0200

    Probleme de generatorURL

--- src/Controller/ShowroomController.php
+++ src/Controller/ShowroomController.php
@@ -2,28 +2,37 @@
 
 namespace App\Controller;
 
+use App\Entity\Sneaker;
 use App\Entity\Showroom;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\Persistence\ManagerRegistry;
-use App\Entity\Sneaker;
+use App\Repository\SneakerRepository;
+use App\Repository\ShowroomRepository;
+
+
+/**
+ * Controleur Showroom
+ * @Route("/showroom")
+ */
 
 class ShowroomController extends AbstractController
 {
-    #[Route('/showroom', name: 'app_showroom')]
+    
+    /**
+    *[Route('/showroom', name: 'app_showroom')]
+    */
+    
     public function indexSneakers(): Response
     {
         $htmlpage = '<!DOCTYPE html>
 <html>
-  <body>Liste des Showrooms :
+  <body>    Liste des Showrooms :
     <ul>
       <li>...La caverne d\'Abdel Aziz...</li>
       <li>...L\'antre de Saad...</li>
       <li>...Le repère de Soumaya...</li>
-
-      ....
-
     </ul>
   </body>
 </html>';
@@ -39,42 +48,82 @@ class ShowroomController extends AbstractController
     }
     
     /**
+     * Lists all shworoom entities.
+     *
+     * @Route("/list", name="showroom_list")
+     */
+    
+    
+    public function listShowroom(ManagerRegistry $doctrine)
+    {
+        $htmlpage = '<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Showroom lists</title>
+    </head>
+    <body>
+        <h1>Showroom list</h1>
+        <p>Voici les la liste des showrooms:</p>
+        <li><a href="/showroom/42">un showroom (42)</a></li>
+        <ul>';
+        
+        $entityManager= $doctrine->getManager();
+        $showrooms = $entityManager->getRepository(Showroom::class)->findAll();
+        foreach($showrooms as $showroom) {
+            $htmlpage .= '<li>
+            <a href="/todo/'.$showroom->getid().'">'.$showroom->getName().'</a></li>';
+        }
+        $htmlpage .= '</ul>';
+        
+        $htmlpage .= '</body></html>';
+        $url = $this->generateUrl( '[inventaire]_show',    ['id' => $showroom->getId()] );
+        
+        
+        return new Response(
+            $htmlpage,
+            Response::HTTP_OK,
+            array('content-type' => 'text/html')
+            );
+    }
+    
+    /**
      * Show a showroom
      *
-     * @Route("/showroom/{id}", name="showroom_show", requirements={"id"="\d+"})
+     * @Route("/{id}", name="showroom_show", requirements={"id"="\d+"})
      *    note that the id must be an integer, above
      *
      * @param Integer $id
      */
-    public function showInventaire(ManagerRegistry $doctrine, $id)
+    public function showInventaire(ManagerRegistry $doctrine, $id): Response
     {
-        $showroomRepo = $doctrine->getRepository(Showroom::class);
-        $showroom = $showroomRepo->find($id);
-        $sneakerRepo = $doctrine->getRepository(Sneaker::class);
-        $sneaker = $sneakerRepo->find($id);
         
+        $showroomrepo = $doctrine->getRepository(Showroom::class);
+        $showroom = $showroomrepo->find($id);
         
         if (!$showroom) {
             throw $this->createNotFoundException('The showroom does not exist');
         }
         
-        $res = '1';
         $res ='<!DOCTYPE html>
         <html>
             <head>
                 <meta charset="UTF-8">
                 <title>Showroom :'.$showroom->getName().' details</title>
             </head>
-        <body>';
-        
-        $res .= '<dt>Sneaker</dt><dd>' . $sneaker->getModel() . '</dd>';
-        $res .= '<dt></dt> <dd> ' . $sneaker->getColor() . '</dd>';
-        
+        <body>
+            <h2>Sneakers Details :</h2>
+            <ul>
+            <dl>    
+        <dt>Sneaker</dt>';
+        $sneakers = $showroom->getRelation();
+        foreach ($sneakers as $sneaker)
+            {
+        $res .= '<dt></dt><dd>' . $sneaker->getModel() .' '. $sneaker->getColor() . '</dd>'; 
         $res .= '</dl>';
         $res .= '</ul></body></html>';
-        
-        $res .= '<p/><a href="' . $this->generateUrl('showroom_index') . '">Back</a>';
-        
+        $res .= '<p/><a href="' . $this->generateUrl('showroom_show', array('slug' => 'showroom')) . '">Back</a>';
+            }
         return new Response('<html><body>'. $res . '</body></html>');
     }
     
